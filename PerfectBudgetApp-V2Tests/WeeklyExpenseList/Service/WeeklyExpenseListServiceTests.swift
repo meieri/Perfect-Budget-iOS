eb0aaae7c54cf4556d7652619310d1f734aaca14
Isaak Meier <isaak.meier@whoop.com>
2020-08-11 19:09:05 -0400

    Fixed service

--- PerfectBudgetApp-V2Tests/WeeklyExpenseList/Service/WeeklyExpenseListServiceTests.swift
+++ PerfectBudgetApp-V2Tests/WeeklyExpenseList/Service/WeeklyExpenseListServiceTests.swift
@@ -14,17 +14,10 @@ class MockTransactionService: TransactionServiceProtocol {
     let managedContext = NSPersistentContainer(name: "Test Container").viewContext
 
     func createTransaction(reason: String, amount: Double) -> Transaction {
-        let entity = NSEntityDescription.entity(forEntityName: "Transaction", in: managedContext)!
-        let transactionManagedObject = NSManagedObject(entity: entity, insertInto: managedContext)
-        transactionManagedObject.setValue(amount, forKey: "amount")
-        transactionManagedObject.setValue(Date(), forKey: "date")
-        transactionManagedObject.setValue(reason, forKey: "reason")
-        do {
-            try managedContext.save()
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
-        return transactionManagedObject as! Transaction
+        let transaction = Transaction()
+        transaction.amount = amount
+        transaction.reason = reason
+        return transaction
     }
 
     func fetchTransactions() -> [Transaction] {
