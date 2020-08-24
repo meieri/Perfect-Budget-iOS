b12b914efde40ecf9ddc1d5a0b39bcc323358a53
Isaak Meier <isaak.meier@whoop.com>
2020-08-23 21:28:50 -0400

    Added uibutton

--- PerfectBudgetApp-V2/Modules/WeeklyExpense/View/WeeklyExpensePageViewController.swift
+++ PerfectBudgetApp-V2/Modules/WeeklyExpense/View/WeeklyExpensePageViewController.swift
@@ -14,6 +14,7 @@ class WeeklyExpensePageViewController: UIPageViewController {
     // view controllers are ordered by increasing week, this week exists at 0
     var orderedWeeklyViewControllers: [UIViewController]!
     var output: WeeklyExpenseViewOutput!
+    var drawerMenuButton = UIButton()
     private var currentIndex: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
